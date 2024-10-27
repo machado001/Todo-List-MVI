481bdb71ae07de6b1722f166bc7177750d066069
machado001 <machadowg@gmail.com>
2024-10-27 00:51:01 -0300

    create datasource with fake data

---
+++ app/src/main/java/com/machado001/todolist/todo/data/MockData.kt
@@ -0,0 +1,74 @@
+package com.machado001.todolist.todo.data
+
+import com.machado001.todolist.core.domain.EmptyResult
+import com.machado001.todolist.core.domain.LocalError
+import com.machado001.todolist.core.domain.Result
+import com.machado001.todolist.todo.domain.Note
+import com.machado001.todolist.todo.domain.NoteDataSource
+import com.machado001.todolist.todo.domain.NoteState
+import io.bloco.faker.Faker
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlin.coroutines.cancellation.CancellationException
+
+//Simulating a database while i don't have a real one.
+class MockData : NoteDataSource {
+    override val notes: Flow<List<Note>> = flow {
+        emit(noteMockDatabase)
+    }
+
+    override suspend fun createNote(note: Note): Result<Note, LocalError> = try {
+        faker.run {
+            val n = Note(
+                id = note.id,
+                title = note.title,
+                description = note.description,
+                date = note.date,
+                state = NoteState.Todo
+            )
+            noteMockDatabase.add(n)
+            Result.Success(n)
+        }
+    } catch (e: Exception) {
+        if (e is CancellationException) throw e
+        Result.Error(LocalError.UNKNOWN_ERROR)
+    }
+
+    override suspend fun updateNote(note: Note): Result<Note, LocalError> = try {
+        noteMockDatabase.first { it.id == note.id }
+            .run {
+                Result.Success(copy(id = id, title = title, description = description, date = date))
+            }
+    } catch (e: Exception) {
+        if (e is CancellationException) throw e
+        Result.Error(LocalError.UNKNOWN_ERROR)
+    }
+
+    override suspend fun deleteNote(note: Note): EmptyResult<LocalError> = try {
+        noteMockDatabase.remove(note)
+        Result.Success(Unit)
+    } catch (e: Exception) {
+        if (e is CancellationException) throw e
+        Result.Error(LocalError.UNKNOWN_ERROR)
+    }
+}
+
+private val faker = Faker()
+
+private val noteMockDatabase = buildList {
+    repeat(30) {
+        val possibleNoteStates = listOf(NoteState.Todo, NoteState.Done, NoteState.InProgress)
+        with(faker) {
+            add(
+                Note(
+                    id = number.digit().toInt(),
+                    title = lorem.characters(7),
+                    description = lorem.characters(30),
+                    date = String.format("dd/MM/yyyy", date.forward()),
+                    state = possibleNoteStates.random()
+                )
+            )
+        }
+    }
+
+}.toMutableList()
--- app/src/main/java/com/machado001/todolist/todo/domain/NoteDataSource.kt
+++ app/src/main/java/com/machado001/todolist/todo/domain/NoteDataSource.kt
@@ -1,12 +1,13 @@
 package com.machado001.todolist.todo.domain
 
+import com.machado001.todolist.core.domain.EmptyResult
 import com.machado001.todolist.core.domain.LocalError
 import com.machado001.todolist.core.domain.Result
 import kotlinx.coroutines.flow.Flow
 
 interface NoteDataSource {
     val notes: Flow<List<Note>>
-    suspend fun updateNote(note: Note): Result<Flow<Note>, LocalError>
-    suspend fun getNote(note: Note): Result<Flow<Note>, LocalError>
-    suspend fun deleteNote(note: Note): Result<Flow<Note>, LocalError>
+    suspend fun createNote(note: Note): Result<Note, LocalError>
+    suspend fun updateNote(note: Note): Result<Note, LocalError>
+    suspend fun deleteNote(note: Note): EmptyResult<LocalError>
 }
