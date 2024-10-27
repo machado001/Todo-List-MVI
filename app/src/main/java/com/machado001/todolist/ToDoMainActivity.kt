e831fbb1e925f3793eddb09524c2ef158c1f7878
machado001 <machadowg@gmail.com>
2024-10-27 01:40:27 -0300

    card imports

--- app/src/main/java/com/machado001/todolist/ToDoMainActivity.kt
+++ app/src/main/java/com/machado001/todolist/ToDoMainActivity.kt
@@ -12,6 +12,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
